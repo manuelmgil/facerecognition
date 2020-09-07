fff39fb00c3f8c338e6657d41539cf97567fd648
Manuel Mukul <manuelmgil.94@gmail.com>
2020-09-07 01:26:30 -0500

    cambios a la 1:26, inicio de proyecto

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Navigation.css"
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            </nav>
+        </div>
+    );
+}
+
+export default Navigation
